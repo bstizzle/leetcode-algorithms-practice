b390ce7bce45135bd8522808ba91253cd4f93825
benyatta <whereyouben@gmail.com>
2021-03-25 17:13:25 -0400

    canJump

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,19 @@
-function canJump(nums) {}
+function canJump(nums) {
+    //Time complexity O(N^2) nested for loops
+    //Space complexity O(N) length of input array
+    let positions = new Array(nums.length).fill(false);
+    positions[0] = true;
+
+    for(let j = 1; j < nums.length; j++){
+        for(let i = 0; i < j; i++){
+            if(positions[i] && i + nums[i] >= j){
+                positions[j] = true;
+                break;
+            }
+        }
+    }
+
+    return positions[positions.length - 1];
+}
 
 module.exports = canJump;
