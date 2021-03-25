3c57500a6f4638b4d1b786f93104fe61811d27fd
benyatta <whereyouben@gmail.com>
2021-03-25 16:07:39 -0400

    more problems

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,3 +1,17 @@
-function rob(nums) {}
+function rob(nums) {
+    //Time complexity O(N), loops over array once
+    //Space complexity O(N), length of input array
+    if(nums.length === 0) return 0;
+    if(nums.length === 1) return nums[0];
+    if(nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    let maxLootAt = [nums[0], Math.max(nums[0], nums[1])]
+
+    for(let i = 2; i < nums.length; i++){
+        maxLootAt.push(Math.max(nums[i] + maxLootAt[i-2], maxLootAt[i-1]))
+    }
+
+    return maxLootAt.pop();
+}
 
 module.exports = rob;
