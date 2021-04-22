5e372a450f5ff7749d7774fc72ae8c0ff9d8e442
benyatta <whereyouben@gmail.com>
2021-04-22 13:50:24 -0400

    coin change

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -1,3 +1,20 @@
-function coinChange(coins, amount) {}
+function coinChange(coins, amount) {
+    //Time complexity O(N^2) due to nested for loops
+    //time complexity O(N) n is length of dp array
+    let dpMinCoins = new Array(amount + 1).fill(Infinity);
+    dpMinCoins[0] = 0;
+
+    for(let i = 1; i < dpMinCoins.length; i++) {
+        for(let j = 0; j < coins.length; j++) {
+            const coinValue = coins[j];
+            if(coinValue <= i) {
+                dpMinCoins[i] = Math.min(dpMinCoins[i - coinValue] + 1, dpMinCoins[i])
+            }
+        }
+    }
+
+    const answer = dpMinCoins[dpMinCoins.length - 1];
+    return answer === Infinity ? -1 : answer;
+}
 
 module.exports = coinChange;
