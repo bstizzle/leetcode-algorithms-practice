74017e6574a2dde9830124a8c2ed642cb33d1c2b
Ben Stein <whereyouben@gmail.com>
2021-04-26 18:55:50 -0400

    maxArea

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -1,3 +1,22 @@
-function maxArea(height) {}
+function maxArea(height) {
+  //time complexity O(N) each index is visisted once
+  //space complexity O(1)
+  let leftP = 0;
+  let rightP = height.length - 1;
+  let maxArea = 0;
+
+  while(leftP < rightP){
+    const currentArea = Math.min(height[leftP], height[rightP]) * (rightP - leftP);
+    maxArea = Math.max(currentArea, maxArea);
+
+    if(height[leftP] < height[rightP]){
+      leftP++
+    } else {
+      rightP--
+    }
+  }
+
+  return maxArea;
+}
 
 module.exports = maxArea;
