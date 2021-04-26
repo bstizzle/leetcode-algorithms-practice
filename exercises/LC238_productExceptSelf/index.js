e11034e9aebf0adb4e2d46af1f6da256ce02128d
Ben Stein <whereyouben@gmail.com>
2021-04-26 18:07:39 -0400

    productExceptSelf

--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,24 @@
-const productExceptSelf = nums => {};
+const productExceptSelf = nums => {
+  //time complexity O(N) because only sequential loops, 2N == N
+  //time complexity O(1) because output array doesnt count
+  let output = nums.map(n => 1);
+  let product = 1;
+
+  //multiply from the left
+  for(let i = 0; i < nums.length; i++){
+    output[i] = output[i] * product;
+    product = product * nums[i];
+  }
+
+  product = 1;
+
+  //multiply from the right
+  for(let j = nums.length - 1; j >= 0; j--){
+    output[j] = output[j] * product;
+    product = product * nums[j];
+  }
+
+  return output;
+};
 
 module.exports = productExceptSelf;
