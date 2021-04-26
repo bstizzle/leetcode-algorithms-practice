0e1b26f6a88e6e16679a16de8d32aee4212c7c63
Ben Stein <whereyouben@gmail.com>
2021-04-26 17:47:11 -0400

    containsDuplicate

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,19 @@
-const containsDuplicate = nums => {};
+const containsDuplicate = nums => {
+  //time complexity O(n) because there is one loop
+  //space complexity O(N) n is the amount of unique values in input array
+  const visitedNums = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if(visitedNums[num]) {
+      return true;
+    } else {
+      visitedNums[num] = true;
+    }
+  }
+
+  return false;
+};
 
 module.exports = containsDuplicate;
