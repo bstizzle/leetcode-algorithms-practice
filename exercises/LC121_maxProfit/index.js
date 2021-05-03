011440737d6a943f9f55f7fa319fc0a6f92e5fa2
Ben Stein <whereyouben@gmail.com>
2021-05-03 16:22:34 -0400

    maxProfit

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -1,3 +1,18 @@
-var maxProfit = function(prices) {};
+var maxProfit = function(prices) {
+  //space complexity O(N) one for loop
+  //space complexity O(1)
+  let maxProfit = 0;
+  let cheapestPrice = prices[0];
+
+  for(let i = 0; i < prices.length; i++) {
+    const price = prices[i]
+    if(price < cheapestPrice) cheapestPrice = price;
+
+    const currentProfit = price - cheapestPrice;
+    maxProfit = Math.max(currentProfit, maxProfit);
+  }
+
+  return maxProfit;
+};
 
 module.exports = maxProfit;
